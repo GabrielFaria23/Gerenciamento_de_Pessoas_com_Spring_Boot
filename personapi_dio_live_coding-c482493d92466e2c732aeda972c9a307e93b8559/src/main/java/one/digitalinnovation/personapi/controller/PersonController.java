cfc6da3d90966872e84902049da8378ee63248ec
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-12-15 10:38:28 -0300

    people register created

--- personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,32 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API Test!";
+
+    private PersonRepository personRepository;
+
+    @Autowired //fazer a declaração de dependencias em um contrutor facilita na hora dos testes unitarios
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID "+ savedPerson.getId())
+                .build();
     }
 }
