6063257669422c0ef1d8446c4c5ef845f13cc84d
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-12-15 13:50:28 -0300

    Improved person creation functionality

--- personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,7 +27,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 }
