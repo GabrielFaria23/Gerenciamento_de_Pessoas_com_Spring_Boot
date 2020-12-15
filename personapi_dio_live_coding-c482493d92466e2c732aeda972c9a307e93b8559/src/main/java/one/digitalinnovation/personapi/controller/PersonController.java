cbf4d4dc1eb6f9ae93976429e82a81a0bef39536
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-12-15 13:56:12 -0300

    Method findAll created

--- personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi_dio_live_coding-c482493d92466e2c732aeda972c9a307e93b8559/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -30,4 +30,9 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
     }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
 }
